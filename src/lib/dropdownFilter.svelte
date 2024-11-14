904ba54fc8a02dc1bccbdd7859d754ce1a91b6df
Jason <jay.jpy.wong@gmail.com>
2024-11-14 22:36:25 +0100

    Feature : Very early version of the dropdown menu component. Still needs reactiveness, probably stores (optional), change the shown vacancies based on filters..

---
+++ src/lib/dropdownFilter.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let selectedOption = "";
+    export let filterLabel = "";
+    export let filter = [];
+</script>
+
+<select bind:value={selectedOption}>
+    <option value="">{filterLabel}</option>
+    {#each filter as filter}
+        <option value={filter.value}>{filter.value}</option>
+    {/each}
+</select>
+
+<h2>De gebruiker kiest : {selectedOption}</h2>
+
+<style>
+    h2 {
+        color: white;
+    }
+    select {
+        background-color: inherit;
+        color: white;
+        border: 1px solid white;
+        padding: 1em 1.5em;
+    }
+</style>
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,41 @@
-<main>
-    
-</main>
+<script>
+    import Dropdown from '$lib/dropdownFilter.svelte';
+
+    const expertises = [
+        { value: "Design" },
+        { value: "UI/UX" },
+        { value: "Graphic" },
+    ];
+
+    const provincies = [
+        { value: "Noord-holland" },
+        { value: "Zuid-holland" },
+        { value: "Utrecht" },
+    ];
+
+    const voertalen = [
+        { value: "Nederlands" },
+        { value: "Engels" },
+        { value: "Chinees" },
+    ]
+
+    let selectedDesign, selectedDevelopment, selectedLanguage = "";
+</script>
+
+<Dropdown
+    bind:selectedOption={selectedDesign}
+    filterLabel="Expertise"
+    filter={expertises} 
+/>
+
+<Dropdown
+    bind:selectedOption={selectedDevelopment}
+    filterLabel="Provincie"
+    filter={provincies} 
+/>
+
+<Dropdown 
+    bind:selectedOption={selectedLanguage}
+    filterLabel="Voertaal"
+    filter={voertalen}
+/>
