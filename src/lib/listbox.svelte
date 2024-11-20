6e71868cf0bd8eb7fd4f42b0e3d577754ac32eed
Jason <jay.jpy.wong@gmail.com>
2024-11-20 22:26:52 +0100

    Test : import component

---
+++ src/lib/listbox.svelte
@@ -0,0 +1,30 @@
+<script>
+    import {
+      Listbox,
+      ListboxButton,
+      ListboxOptions,
+      ListboxOption,
+    } from "@rgossiaux/svelte-headlessui";
+  
+    const people = [
+      { id: 1, name: "Durward Reynolds", unavailable: false },
+      { id: 2, name: "Kenton Towne", unavailable: false },
+      { id: 3, name: "Therese Wunsch", unavailable: false },
+      { id: 4, name: "Benedict Kessler", unavailable: true },
+      { id: 5, name: "Katelyn Rohan", unavailable: false },
+    ];
+  
+    let selectedPerson = people[0];
+  </script>
+  
+  <Listbox bind:value={selectedPerson}>
+    <ListboxButton>{selectedPerson.name}</ListboxButton>
+    <ListboxOptions>
+      {#each people as person (person.id)}
+        <ListboxOption value={person} disabled={person.unavailable}>
+          {person.name}
+        </ListboxOption>
+      {/each}
+    </ListboxOptions>
+  </Listbox>
+  Styling
