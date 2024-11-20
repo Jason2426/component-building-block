73774a2ec43f78270c8d7c913dccc8d045bc69ad
Jason <jay.jpy.wong@gmail.com>
2024-11-20 22:27:34 +0100

    Feature : Vacaturepagina filters

---
+++ src/routes/vacatures/+page.svelte
@@ -0,0 +1,74 @@
+<script>
+    import Select from "$lib/selectDropdown.svelte";
+
+
+    const expertises = [
+        { value: "Development" },
+        { value: "Design" },
+        { value: "Traffic" },
+        { value: "Management" },
+    ];
+
+    const soorten = [
+        { value: "Full-time" },
+        { value: "Part-time" },
+        { value: "Min-max" },
+        { value: "Stage" },
+    ];
+
+    const provincies = [
+        { value: "Noord-holland" },
+        { value: "Zuid-holland" },
+        { value: "Utrecht" },
+        { value: "Gelderland" },
+        { value: "Noord-Brabant" },
+        { value: "Limburg" },
+        { value: "Zeeland" },
+    ];
+
+    const voertalen = [
+        { value: "Nederlands" },
+        { value: "Engels" },
+        { value: "Spaans" },
+        { value: "Duits" },
+        { value: "Chinees" },
+    ];
+
+    let selectedExpertise, selectedType, selectedProvince, selectedLanguage = "";
+</script>
+
+<main>
+    <section class="filters">
+        <Select
+        bind:selectedOption={selectedExpertise}
+        filterLabel="Expertise"
+        filter={expertises}
+        />
+        <Select
+            bind:selectedOption={selectedType}
+            filterLabel="Soort"
+            filter={soorten}
+        />
+        <Select
+            bind:selectedOption={selectedProvince}
+            filterLabel="Locatie"
+            filter={provincies}
+        />
+        <Select
+            bind:selectedOption={selectedLanguage}
+            filterLabel="Voertaal"
+            filter={voertalen}
+        />
+    </section>
+</main>
+
+<style>
+
+    .filters {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 1.5em;
+    }
+
+</style>
